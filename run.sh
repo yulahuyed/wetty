040b68fc6afc6911215f878f18745519a7f3a762
yulahuyed <yulahuyed@vpsorg.top>
2018-10-20 15:35:14 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+envsubst < /tmp/passwd_template > /tmp/passwd
+export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
+wetty -p 8080
